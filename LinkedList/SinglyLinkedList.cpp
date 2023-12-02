130d7cb3331de6bcaab2bbcf13888966d37b9735
Junyu Tang <144630354+Rh0-aias@users.noreply.github.com>
2023-12-02 14:56:47 -0800

    Update SinglyLinkedList.cpp

--- LinkedList/SinglyLinkedList.cpp
+++ LinkedList/SinglyLinkedList.cpp
@@ -1,15 +1,24 @@
 class Node{
     public:
+        //Constructors
+        Node() : val(0), next(nullptr) {}
+        Node(double init_val, Node* init_next = nullptr): val(init_val), next(init_next) {}
+        //Deconstructor
+        ~Node(){}
+        //Methods
+        double getValue(){ return val; }
+        void setValue(double val){ this->val = val; }
+        Node* getNext(){ retun next; }
+        void setNext(Node* Nextnode){ next = Nextnode; }
+    private:
         double val;
         Node* next;
-    //Constructors
-    Node() : val(0), next(nullptr) {}
-    Node(double init_val, Node* init_next = nullptr): val(init_val), next(init_next) {}
-    //Deconstructor
-    ~Node(){}
 }
 
-int main() {
+
+
+
+int main(){
 
 
 }
